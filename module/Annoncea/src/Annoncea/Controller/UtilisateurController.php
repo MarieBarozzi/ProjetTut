b2114c4869129dd6c42bd55f6ebe1b208937b7e3
Julien Hoffmann <julpark.site@gmail.com>
2013-09-19 18:19:25 +0200

    suppression de la fonction crypter()

--- module/Annoncea/src/Annoncea/Controller/UtilisateurController.php
+++ module/Annoncea/src/Annoncea/Controller/UtilisateurController.php
@@ -152,32 +152,5 @@ class UtilisateurController extends AbstractActionController
         }
         return array('form' => $form);
     }
-       
-    public function crypter($maCleDeCryptage='', $maChaineACrypter)
-    {
-
-        if($maCleDeCryptage==""){
-            $maCleDeCryptage=$GLOBALS['PHPSESSID'];
-        }
-        $maCleDeCryptage = md5($maCleDeCryptage);
-        $letter = -1;
-        $newstr = '';
-        $strlen = strlen($maChaineACrypter);
-
-        for($i = 0; $i < $strlen; $i++ ){
-            $letter++;
-        if ( $letter > 31 ){
-        $letter = 0;
-        }
-
-        $neword = ord($maChaineACrypter{$i}) + ord($maCleDeCryptage{$letter});
-
-        if ( $neword > 255 ){
-        $neword -= 256;
-        }
-        $newstr .= chr($neword);
-        }
-        return base64_encode($newstr);
-    } 
     
 }
